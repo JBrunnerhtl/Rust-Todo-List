281e9e6ba9597a3163b95d848c65e680d52b5495
JBrunnerhtl <j.brunner1@students.htl-leonding.ac.at>
2025-05-26 20:01:14 +0200

    Now can add to json

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,11 @@
 use clap::{Parser, Subcommand};
 use serde::{Deserialize, Serialize};
 use std::{fs, path::Path, io::Write};
+use std::fs::OpenOptions;
+use serde_json::Value;
 #[derive(Serialize, Deserialize, Debug)]
 struct Message {
+    id: usize,
     message: String,
 }
 enum Command {
@@ -17,17 +20,19 @@ enum Command {
 }
 impl Message {
     fn new(message: String) -> Self {
-        Self { message }
+        Self { message, id: 0 }
+
     }
 
     fn push_to_json(&self, path: &Path){
         if path.exists() {
             let json_data = fs::read_to_string(path).expect("Something went wrong reading the file");
             let mut result_array : Vec<Message> = Vec::new();
-            let mut result_single;
+            let mut result_single: Message;
             if serde_json::from_str::<Message>(&json_data).is_ok() {
                 result_single = serde_json::from_str::<Message>(&json_data).unwrap();
                 println!("{}", result_single.message);
+                result_array.push(result_single);
             }
             else if serde_json::from_str::<Vec<Message>>(&json_data).is_ok() {
 
@@ -37,6 +42,13 @@ impl Message {
                 })
             }
 
+            let new_message: String = format!(r#"{{"id":{},"message":"{}"}}"#, result_array.len()+1, self.message);
+            let parsed: Message = serde_json::from_str(&new_message).unwrap();
+            result_array.push(parsed);
+            let result_array = serde_json::to_string_pretty(&result_array).unwrap();
+
+            let mut file = OpenOptions::new().write(true).truncate(true).create(true).open(path).unwrap();
+            file.write_all(result_array.as_bytes()).unwrap();
 
         }
 
