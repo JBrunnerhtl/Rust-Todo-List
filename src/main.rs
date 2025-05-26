22e89cc43ca612e02c5ff1e35e10042407da1c03
JBrunnerhtl <j.brunner1@students.htl-leonding.ac.at>
2025-05-26 19:02:48 +0200

    Now can read from json and print it to the console

--- src/main.rs
+++ src/main.rs
@@ -20,16 +20,26 @@ impl Message {
         Self { message }
     }
 
-    fn push_to_json(&self, path: &Path) -> Vec<Message>{
+    fn push_to_json(&self, path: &Path){
         if path.exists() {
-            let json_data = fs::read_to_string(path).unwrap();
-            let a = serde_json::from_str::<Vec<Message>>(&json_data).unwrap_or_default();
-            println!("{}", json_data);
-            Vec::new()
+            let json_data = fs::read_to_string(path).expect("Something went wrong reading the file");
+            let mut result_array : Vec<Message> = Vec::new();
+            let mut result_single;
+            if serde_json::from_str::<Message>(&json_data).is_ok() {
+                result_single = serde_json::from_str::<Message>(&json_data).unwrap();
+                println!("{}", result_single.message);
+            }
+            else if serde_json::from_str::<Vec<Message>>(&json_data).is_ok() {
+
+                result_array = serde_json::from_str::<Vec<Message>>(&json_data).unwrap_or_default();
+                result_array.iter().for_each(|msg|{
+                    println!("{}", msg.message);
+                })
+            }
+
+
         }
-        else {
-            Vec::new()
-        }
+
     }
 
     fn delete_message(&self, path: &Path) {
